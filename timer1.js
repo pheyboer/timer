23eb6fd2eff9b73910c0e1624b0b4944b8b4e161
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-17 16:12:53 -0400

    added timer1.js

--- timer1.js
+++ timer1.js
@@ -2,10 +2,29 @@
 // Beep sound not working on PC
 // process.stdout.write('Beep!\n');
 
+
+// Implement a timer to beep after a specified amount of time has passed
+// User can specifiy an unlimited number of alarms using Command Line Arugments
+
+// Set interval times in seconds
+// Command line arugments are given as seconds. Convert to miliseconds
+// use setTimeout
+
 // Take command line arugments
 const args = process.argv.slice(2);
+
+// Get interval from Command Line and map to numbers
+
+const intervalSeconds = args.map(Number);
 
 // Function for beep sound
 const beep = function() {
   process.stdout.write('Beep!\n');
 };
+
+// Set beep for specified times
+
+intervalSeconds.forEach(interval => {
+  const intervalMiliseconds = interval * 1000;
+  setTimeout(beep, intervalMiliseconds);
+});
