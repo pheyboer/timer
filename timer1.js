d429958858eb5edbbb0294f727628e5b48d74992
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-17 18:28:54 -0400

    added edge cases if else statement

--- timer1.js
+++ timer1.js
@@ -15,16 +15,28 @@ const args = process.argv.slice(2);
 
 // Get interval from Command Line and map to integer
 
-const intervalSeconds = args.map(Number);
+const timeSeconds = args.map(Number);
 
 // Function for beep sound. Writes out Beep!
 const beep = function() {
   process.stdout.write('Beep!\n');
 };
 
-// Set beep for specified times
+// Set beep for specified times and handle edge cases
+// Wrapped the setTimeout for...each loop in and if...else statement for handling conditions
+// Edge cases for 0, negative numbers, and NaN
 
-intervalSeconds.forEach(interval => {
-  const intervalMiliseconds = interval * 1000;
-  setTimeout(beep, intervalMiliseconds);
-});
+if (timeSeconds.length === 0) {
+  console.log(`No Number Provided. Please add some Positive Numbers for the Timer to work!`);
+} else {
+  timeSeconds.forEach(interval => {
+    if (interval < 0) {
+      console.log(`${interval} is negative. Please use a Positive Number`);
+    } else if (isNaN(interval)) {
+      console.log(`${interval} is Not a Number. Please use a number`);
+    } else {
+      const timeMiliseconds = interval * 1000;
+      setTimeout(beep, timeMiliseconds);
+    }
+  });
+}
